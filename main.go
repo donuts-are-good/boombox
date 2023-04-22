7fc71bb3ec425f0643eaf7f20e4a8cc23c0b5312
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-04-21 22:55:14 -0500

    playing playlist with listener count

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/faiface/beep"
@@ -17,8 +21,10 @@ import (
 )
 
 var (
-	playlist string
-	port     int
+	playlist      string
+	port          int
+	listenerCount int
+	listenerMutex sync.Mutex
 )
 
 func main() {
@@ -29,13 +35,14 @@ func main() {
 	}
 
 	rootCmd.Flags().StringVar(&playlist, "playlist", "", "Path to the playlist file (m3u)")
-	rootCmd.Flags().IntVar(&port, "port", 12401, "Port on which the server will run")
+	rootCmd.Flags().IntVar(&port, "port", 42001, "Port on which the server will run")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
+
 func runServer(cmd *cobra.Command, args []string) {
 	flag.Parse()
 
@@ -51,26 +58,93 @@ func runServer(cmd *cobra.Command, args []string) {
 }
 
 func streamHandler(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open(playlist)
+	listenerMutex.Lock()
+	listenerCount++
+	listenerMutex.Unlock()
+
+	defer func() {
+		listenerMutex.Lock()
+		listenerCount--
+		listenerMutex.Unlock()
+	}()
+
+	pathMap := make(map[string]struct{})
+
+	for {
+		paths, err := parseM3u(playlist)
+		if err != nil {
+			log.Println("error parsing m3u: ", err)
+		}
+
+		for _, path := range paths {
+			if _, ok := pathMap[path]; !ok {
+				pathMap[path] = struct{}{}
+
+				listenerMutex.Lock()
+				log.Printf("Listeners connected: %d  Playing track: %s\n", listenerCount, path)
+				listenerMutex.Unlock()
+
+				f, err := os.Open(path)
+				if err != nil {
+					log.Println("error opening file: ", err)
+					continue
+				}
+
+				streamer, format, err := mp3.Decode(f)
+				if err != nil {
+					log.Println("error decoding file: ", err)
+					f.Close()
+					continue
+				}
+
+				speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+				done := make(chan struct{})
+				speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+					close(done)
+				})))
+
+				select {
+				case <-done:
+					streamer.Close()
+					f.Close()
+				case <-r.Context().Done():
+					streamer.Close()
+					f.Close()
+					return
+				}
+			}
+		}
+	}
+}
+
+func parseM3u(filename string) ([]string, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var paths []string
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(line, "#") || line == "" {
+			continue
+		}
+
+		path := line
+		if !filepath.IsAbs(path) {
+			dir := filepath.Dir(filename)
+			path = filepath.Join(dir, line)
+		}
+
+		paths = append(paths, path)
 	}
 
-	streamer, format, err := mp3.Decode(f)
-	if err != nil {
-		log.Fatal(err)
+	if err := scanner.Err(); err != nil {
+		return nil, err
 	}
-	defer streamer.Close()
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-	done := make(chan struct{})
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-		close(done)
-	})))
-
-	select {
-	case <-done:
-	case <-r.Context().Done():
-	}
+	return paths, nil
 }
